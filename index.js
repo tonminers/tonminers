31c07569931f45a29f1ad827cb88c7f2935eb0cb
Ton Miners <tonminersrobot@gmail.com>
2024-10-21 01:07:15 +0200

    test

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ $( document ).ready(function() {
     $("#first_name").html(userData.user.first_name);
 
     const tg = Telegram.WebApp.MainButton;
-    tg.setText("⚒️ New TON Mining Power Units (TMPU)");
+    tg.setText("🟢 New TON Mining Power Units (TMPU)");
     tg.show();
 });

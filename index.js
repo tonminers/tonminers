78b944b208485a904296230a450da3419e6c7c14
Ton Miners <tonminersrobot@gmail.com>
2024-10-22 00:14:25 +0200

    test

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 $( document ).ready(function() {
-    const params = new URLSearchParams(Telegram.WebApp.initData);
-    const userData = Object.fromEntries(params);
-    userData.user = JSON.parse(userData.user);
+    // const params = new URLSearchParams(Telegram.WebApp.initData);
+    // const userData = Object.fromEntries(params);
+    // userData.user = JSON.parse(userData.user);
 
-    $("#first_name").html(userData.user.first_name);
+    // $("#first_name").html(userData.user.first_name);
 
-    const tg = Telegram.WebApp;
+    // const tg = Telegram.WebApp;
 
-    tg.MainButton.setText("⚪️ Compound");
-    tg.MainButton.show();
+    // tg.MainButton.setText("⚪️ Compound");
+    // tg.MainButton.show();
 
-    tg.SecondaryButton.setText("⚪️ Withdraw");
-    tg.SecondaryButton.show();
-    tg.SecondaryButton.color = tg.MainButton.color;
-    tg.SecondaryButton.textColor = tg.MainButton.textColor;
+    // tg.SecondaryButton.setText("⚪️ Withdraw");
+    // tg.SecondaryButton.show();
+    // tg.SecondaryButton.color = tg.MainButton.color;
+    // tg.SecondaryButton.textColor = tg.MainButton.textColor;
 
     // tg.BackButton.show();
 });
